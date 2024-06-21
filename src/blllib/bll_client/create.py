8a50839c2472c9c708ab15a37d43907d28646465
shiron <shiron4710.dev@gmail.com>
2024-06-21 17:00:54 +0900

    wip

--- src/blllib/bll_client/create.py
+++ src/blllib/bll_client/create.py
@@ -3,5 +3,5 @@ from typing import Callable
 import blllib
 
 
-def create(on_start: Callable, on_update: Callable[[list[str]], None], algo_info: blllib.AlgorithmAlgorithmData):
+def create(on_start: Callable, on_update: Callable[[list[str]], None], algo_info: blllib.AlgorithmAlgorithmInfomation):
     return blllib.BLL(on_start=on_start, on_update=on_update, algo_info=algo_info)
